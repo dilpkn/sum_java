b5c919499c82dacb70d678c2d0207661cfb25cb5
dilpkn <dianawb19@gmail.com>
2024-11-19 16:05:51 +0300

    first

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module org.example.sum_les_1 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens org.example.sum_les_1 to javafx.fxml;
+    exports org.example.sum_les_1;
+}
